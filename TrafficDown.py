7962e29ac31d23d6db549b930af6d9dfbf26d209
wheelchairy <lain@iwakurahome.ru>
2025-03-27 16:04:49 +0300

    credits

--- TrafficDown.py
+++ TrafficDown.py
@@ -300,10 +300,10 @@ def main():
             CTkLabel(window, text='Github @Sonys9 | tt @взломщик | tg @freedomleaker2', 
                     font=('Arial Black', 8), bg_color='#2B2B2B').place(x=screen_width//2-120, y=screen_height-40)
             
-            CTkLabel(window, text='Многопоточность: Github @cryptexctl | tg @systemxpore', 
+            CTkLabel(window, text='Многопоточность: gh @cryptexctl | tg @systemxpore', 
                     font=('Arial Black', 8), bg_color='#2B2B2B').place(x=screen_width//2-120, y=screen_height-30)
             
-            CTkLabel(window, text='Переработанная система серверов: Github @cryptexctl | tg @systemxpore', 
+            CTkLabel(window, text='Server system rework: gh @cryptexctl | tg @systemxpore', 
                     font=('Arial Black', 8), bg_color='#2B2B2B').place(x=screen_width//2-120, y=screen_height-20)
             
             startbtn = CTkButton(window, text='Есть трафик', command=start_eat_ctkinter, 
